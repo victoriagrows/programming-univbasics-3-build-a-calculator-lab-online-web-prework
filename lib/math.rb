0374d3f35cd814422772b8885312885f533f377c
Victoria Cushing <victoriaccushing@gmail.com>
2019-06-25 04:11:23 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,26 +1,27 @@
 def addition(num1, num2)
   num1 + num2
 end
-addition(5,4)
+addition(5, 4)
 
 def subtraction(num1, num2)
   num1 - num2
 end
-subtraction(10,5)
+subtraction(10, 5)
 
 def division(num1, num2)
   num1 / num2
 end
-division(50,2)
+division(50, 2)
 
 def multiplication(num1, num2)
   num1 * num2
 end
-multiplication(4,30)
+multiplication(4, 30)
 
 def modulo(num1, num2)
-
+  num1 / num2
 end
+mudulo(34, 5)
 
 def square_root(num)
 
